25cbef5dc17072420bf09770d5d3575e7ac51fb5
Urbaexyz <zaifgibran9@gmail.com>
2021-08-26 21:17:53 +0700

    aruga to urbae

--- index.js
+++ index.js
@@ -6,42 +6,42 @@ const figlet = require('figlet')
 const fs = require('fs-extra')
 const HandleMsg = require('./HandleMsg')
 
-const start = async (aruga = new aruga()) => {
+const start = async (urbae = new urbae()) => {
     console.log(color('------------------------------------------------------------------------', 'white'))
     console.log(color(figlet.textSync('Urbaeexyz Bot', { font: 'Ghost', horizontalLayout: 'default' })))
     console.log(color('------------------------------------------------------------------------', 'white'))
     console.log(color('[CREATOR]', 'aqua'), color('Thoriq Azzikra', 'magenta'))
     console.log(color('[BOT]', 'aqua'), color('URBAE BOT is now Online!', 'magenta'))
     console.log(color('[VER]', 'aqua'), color('2.5.8', 'magenta'))
-    aruga.onStateChanged((state) => {
+    urbae.onStateChanged((state) => {
         console.log(color('-> [STATE]'), state)
-        if (state === 'CONFLICT') aruga.forceRefocus()
-        if (state === 'UNPAIRED') aruga.forceRefocus()
+        if (state === 'CONFLICT') urbae.forceRefocus()
+        if (state === 'UNPAIRED') urbae.forceRefocus()
     })
 
-    aruga.onAddedToGroup(async (chat) => {
-        await aruga.sendText(chat.groupMetadata.id, 'Terima kasih sudah memasukkan bot kedalam grup kalian')
-        await aruga.leaveGroup(chat.groupMetada.id)
+    urbae.onAddedToGroup(async (chat) => {
+        await urbae.sendText(chat.groupMetadata.id, 'Terima kasih sudah memasukkan bot kedalam grup kalian')
+        await urbae.leaveGroup(chat.groupMetada.id)
     })
 
-    aruga.onGlobalParticipantsChanged((async (heuh) => {
-        await welcome(aruga, heuh)
-        left(aruga, heuh)
+    urbae.onGlobalParticipantsChanged((async (heuh) => {
+        await welcome(urbae, heuh)
+        left(urbae, heuh)
     }))
 
-    aruga.onMessage((message) => {
-        HandleMsg(aruga, message)
+    urbae.onMessage((message) => {
+        HandleMsg(urbae, message)
     })
 
-    aruga.onIncomingCall(async (callData) => {
+    urbae.onIncomingCall(async (callData) => {
         // ketika seseorang menelpon nomor bot akan mengirim pesan
-        await aruga.sendText(callData.peerJid, 'Maaf sedang tidak bisa menerima panggilan.\n\n-bot')
+        await urbae.sendText(callData.peerJid, 'Maaf sedang tidak bisa menerima panggilan.\n\n-bot')
             .then(async () => {
                 // bot akan memblock nomor itu
-                await aruga.contactBlock(callData.peerJid)
+                await urbae.contactBlock(callData.peerJid)
             })
     })
 }
 create(options(start))
-    .then((aruga) => start(aruga))
+    .then((urbae) => start(urbae))
     .catch((err) => console.error(err))
