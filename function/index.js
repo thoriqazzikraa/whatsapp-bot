5e8f1d0b84af33523fb0de7971eeac7808587c2b
Urbaexyz <zaifgibran9@gmail.com>
2021-10-26 21:02:10 +0700

    qr quality

--- function/index.js
+++ function/index.js
@@ -38,9 +38,8 @@ const processTime = (timestamp, now) => {
 const options = (start) => {
     const options = {
         sessionId: 'URBAE',
-	headless: true,
+		headless: true,
         qrTimeout: 0,
-	qrQuality: 0.1,
         authTimeout: 0,
         restartOnCrash: start,
         cacheEnabled: false,
